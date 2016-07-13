ad60847d4b7f88ae4aee258de7a0a7b892f07f10
arthur.chang <arthur.chang@afusion.com>
2016-07-13 10:06:13 +0800

    First CheckIn

---
+++ AspNetCoreSample.UT/Class.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreSample.UT
+{
+    public interface IMy
+    {
+        Task<string> DoAsync(string arg);
+    }
+
+    [TestFixture]
+    public class Class
+    {
+        [Test]
+        public async Task TestExpectedToPass()
+        {
+            string output = "";
+            var mock = new Mock<IMy>();
+            mock.Setup(o => o.DoAsync(It.IsAny<string>())).Returns(Task.FromResult<string>(output));
+            IMy myObj = mock.Object;
+
+            string actual = await myObj.DoAsync("");
+
+            Assert.AreEqual(output, actual);
+        }
+    }
+}
---
+++ AspNetCoreSample.WebAPI/Startup.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreSample.WebAPI
+{
+    public partial class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            ConfigureAuth(app);
+
+            app.UseMvc();
+        }
+
+        // The secret key every token will be signed with.
+        // Keep this safe on the server!
+        private static readonly string secretKey = "mysupersecret_secretkey!123";
+
+        private void ConfigureAuth(IApplicationBuilder app)
+        {
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                // The signing key must match!
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+
+                // Validate the JWT Issuer (iss) claim
+                ValidateIssuer = true,
+                ValidIssuer = "ExampleIssuer",
+
+                // Validate the JWT Audience (aud) claim
+                ValidateAudience = true,
+                ValidAudience = "ExampleAudience",
+
+                // Validate the token expiry
+                ValidateLifetime = true,
+
+                // If you want to allow a certain amount of clock drift, set that here:
+                ClockSkew = TimeSpan.Zero
+            };
+
+            app.UseJwtBearerAuthentication(new JwtBearerOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                TokenValidationParameters = tokenValidationParameters
+            });
+        }
+    }
+}
